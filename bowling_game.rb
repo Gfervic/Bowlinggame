6b3296756fc90a4209781cf410b367d1a0e22f82
gon.fervic <gon.fervic@gmail.com>
2018-02-28 04:18:42 +0100

    add all logic for score calculation - based on the roll [index and positi [frame]]. If a spare (10 within a frame) it adds up next first_in_frame to that frame

--- bowling_game.rb
+++ bowling_game.rb
@@ -2,16 +2,45 @@ class Game
 
   def initialize
     @rolls = []
-    @score = 0
   end
 
   def roll(pins)
     @rolls << pins
-    @score = score + pins
   end
 
   def score
-    @score
+    score = 0
+    frame = 0
+    first_in_frame = 0
+    while frame < 10
+      if spare?(first_in_frame)
+        score += 10 + @rolls[first_in_frame + 2]
+        first_in_frame += 2
+      else
+        score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
+        first_in_frame += 2
+      end
+      frame +=1
+    end
+    score
   end
+
+#spare method
+  def spare?(first_in_frame)
+    @rolls[first_in_frame] + @rolls[first_in_frame + 1] == 10
+  end
+
+
+
+
+  #   @rolls.each do |roll|
+  #     @score += roll
+  #   end
+  #   score
+  # end
+
+
+
+
 
 end
--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -4,18 +4,19 @@ require_relative 'bowling_game'
 
 describe 'bowling game behaviour' do
 
-  #1test
-
   before do
     @game = Game.new
   end
 
+  #1test
   it 'can create a new game' do
   end
 
   it 'should roll' do
     @game.roll(0)
   end
+
+  #2test
 
   def roll_many(n, pins)
     n.times do
@@ -28,11 +29,22 @@ describe 'bowling game behaviour' do
     expect(@game.score).to eq 0
   end
 
-  #2test
-
   it 'can roll all ones' do
     roll_many(20,1)
     expect(@game.score).to eq 20
+  end
+
+  #3rd test
+  it 'can roll a /' do
+    roll_spare
+    @game.roll(3)
+    roll_many(17,0)
+    expect(@game.score).to eq 16
+  end
+
+  def roll_spare
+    @game.roll(5)
+    @game.roll(5) #spare
   end
 
 
